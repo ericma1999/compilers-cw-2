138c704fced12e50b057871943057b85c400e840
Eric Ma <official.passawis@gmail.com>
2021-04-12 20:24:00 +0700

    check tests

--- comp0012-coursework_files/src/comp0012/target/DynamicVariableFolding.java
+++ comp0012-coursework_files/src/comp0012/target/DynamicVariableFolding.java
@@ -1,28 +1,28 @@
 package comp0012.target;
 
 public class DynamicVariableFolding {
-    // public int methodOne() {
-    //     int a = 42;
-    //     int b = (a + 764) * 3;
-    //     a = b - 67;
-    //     return b + 1234 - a;
-    // }
+    public int methodOne() {
+        int a = 42;
+        int b = (a + 764) * 3;
+        a = b - 67;
+        return b + 1234 - a;
+    }
 
-    // public boolean methodTwo() {
-    //     int x = 12345;
-    //     int y = 54321;
-    //     System.out.println(x < y);
-    //     y = 0;
-    //     return x > y;
-    // }
+    public boolean methodTwo() {
+        int x = 12345;
+        int y = 54321;
+        System.out.println(x < y);
+        y = 0;
+        return x > y;
+    }
 
-    // public int methodThree() {
-    //     int i = 0;
-    //     int j = i + 3;
-    //     i = j + 4;
-    //     j = i + 5;
-    //     return i * j;
-    // }
+    public int methodThree() {
+        int i = 0;
+        int j = i + 3;
+        i = j + 4;
+        j = i + 5;
+        return i * j;
+    }
     
     // public int methodFour(){
     //     int a = 534245;
